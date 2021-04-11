7aad83512cbdadecd4d9de5acb55043bb452229b
VitaliyOleinik <oleynik624@gmail.com>
2021-04-11 20:46:46 +0600

    y = x^3

---
+++ WorkWithAlgorithms/src/PowToThird.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PowToThird {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        double l = 1;
+        double r = x;
+        double eps = 0.000001;
+        while (r - l > eps){
+            double m = (l + r) / 2.0;
+            if (m * m * m > x){
+                r = m;
+            } else {
+                l = m;
+            }
+        }
+
+        System.out.printf("%.10f", r);
+    }
+}
